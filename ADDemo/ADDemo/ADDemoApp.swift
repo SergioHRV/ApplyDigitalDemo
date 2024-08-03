b0633c3f1691691e5a689b7a7ee930f1bff716d0
Sergio Ramirez <sramirez@bitgraylabs.com>
2024-08-03 18:10:00 -0500

    Add base project

---
+++ ADDemo/ADDemo/ADDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADDemoApp.swift
+//  ADDemo
+//
+//  Created by Sergio Ramirez on 3/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ADDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADDemo/ADDemoUITests/ADDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ADDemoUITestsLaunchTests.swift
+//  ADDemoUITests
+//
+//  Created by Sergio Ramirez on 3/08/24.
+//
+
+import XCTest
+
+final class ADDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
